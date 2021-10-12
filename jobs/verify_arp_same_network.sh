f0faa78ddcf3ed2b2f4ebc8fddaf99bd76c10a58
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-13 00:57:46 +1100

    test run

--- jobs/verify_arp_same_network.sh
+++ jobs/verify_arp_same_network.sh
@@ -7,7 +7,7 @@ docker logs docker-client-1
 ((docker exec -t docker-client-1 tcpdump -n udp) > client-tcpdump.txt)&
 sleep 5
 
-echo DISPLAY=:1 python3 /home/ubuntu/selenium/github-resolve-browser-cache.py | su - ubuntu
+docker exec -t docker-client-1 sh -c "DISPLAY=:1 python3 /home/ubuntu/selenium/github-resolve-browser-cache.py | su - ubuntu"
 
 PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
 docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
