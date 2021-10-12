4c8565cd79947a11b8e5877c1a62445ee93553db
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-12 19:00:03 +1100

    fix bad container name

--- jobs/verify_arp_same_network.sh
+++ jobs/verify_arp_same_network.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 docker ps -a
-((docker exec -t docker_client tcpdump -n) > client-tcpdump.txt)&
-((docker exec -t docker_client host www.google.com) > client-host.txt)&
-PID_TCPDUMP=$(docker exec -t docker_client pidof tcpdump)
-docker exec -t docker_client sh -c "kill $PID_TCPDUMP"
+((docker exec -t docker-client-1 tcpdump -n) > client-tcpdump.txt)&
+((docker exec -t docker-client-1 host www.google.com) > client-host.txt)&
+PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
+docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
 
 cat client-tcpdump.txt
 cat client-host.txt
