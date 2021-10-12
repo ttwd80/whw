529d5127070a071ec9a3574f239e26aeed63e405
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-12 23:34:33 +1100

    debugging why PID_TCPDUMP is empty

--- jobs/verify_arp_same_network.sh
+++ jobs/verify_arp_same_network.sh
@@ -3,6 +3,8 @@ docker ps -a
 docker logs docker-client-1
 ((docker exec -t docker-client-1 tcpdump -n) > client-tcpdump.txt)&
 ((docker exec -t docker-client-1 host www.google.com) > client-host.txt)&
+docker exec -t docker-client-1 ps -ef
+docker exec -t docker-client-1 pidof tcpdump
 PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
 docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
 
