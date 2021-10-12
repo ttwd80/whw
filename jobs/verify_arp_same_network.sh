e24acf3af176ac43571abca220ab76e704fb6d61
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-12 18:58:44 +1100

    test

--- jobs/verify_arp_same_network.sh
+++ jobs/verify_arp_same_network.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 docker ps -a
-sleep 30
+((docker exec -t docker_client tcpdump -n) > client-tcpdump.txt)&
+((docker exec -t docker_client host www.google.com) > client-host.txt)&
+PID_TCPDUMP=$(docker exec -t docker_client pidof tcpdump)
+docker exec -t docker_client sh -c "kill $PID_TCPDUMP"
+
+cat client-tcpdump.txt
+cat client-host.txt
