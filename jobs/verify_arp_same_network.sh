dff57547b2fd8e49691879e4c3f49ab8d05118ee
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-13 17:43:39 +1100

    looks like ubuntu user can run google-chrome

--- jobs/verify_arp_same_network.sh
+++ jobs/verify_arp_same_network.sh
@@ -4,9 +4,7 @@ docker ps -a
 
 ((docker exec -t docker-client-1 tcpdump -n udp) > client-tcpdump.txt)&
 
-docker exec -t docker-client-1 sh -c "echo whoami | su - ubuntu"
-docker exec -t docker-client-1 sh -c "echo 'export DISPLAY=:1 && google-chrome' | su - ubuntu"
-# docker exec -t docker-client-1 sh -c "DISPLAY=:1 python3 /home/ubuntu/selenium/github-resolve-browser-cache.py | su - ubuntu"
+docker exec -t docker-client-1 sh -c "python3 /home/ubuntu/selenium/github-resolve-browser-cache.py | su - ubuntu"
 
 PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
 docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
