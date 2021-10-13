d150ae7548d44a400e6ec6e02bab2665430ec24e
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-14 02:56:55 +1100

    different process, no cache even within time limit

---
+++ docker/client/selenium/github-resolve-browser-no-cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from selenium.webdriver import Chrome
+from time import strftime
+import time
+
+driver = Chrome()
+url = "https://github.com/favicon.ico"
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+driver.quit()
+
+driver = Chrome()
+time.sleep(55)
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+driver.quit()
+
+driver = Chrome()
+time.sleep(10)
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+driver.quit()
