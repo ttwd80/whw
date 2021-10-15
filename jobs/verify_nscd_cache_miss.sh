dab1a70653a7797f780c11450946d2b104c54b22
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-16 03:34:24 +1100

    nscd cache miss

---
+++ jobs/verify_nscd_cache_miss.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -e
+
+# If a nscd is running, Google Chrome will try to get a cached value from nscd
+
+# 2 tests before the first browser requests https://www.google.com/favicon.ico
+# - 0 current number of cached values reported by nscd -g
+# - NSCD_HOSTS_ENTRY should be 0
+
+# First set of requests to https://www.google.com/favicon.ico
+
+# Verify:
+# - 1 DNS request uncached
+# - Cached entry count > 0 (www.google.com,  accounts.google.com, etc)
+# - www.google.com should be in /var/cache/nscd/hosts
+
+# invalidate the hosts cache
+# nscd --invalidate=hosts
+
+# Verify
+# - 1 DNS request uncached - unchanged
+# - Cached entry count == 0
+# - www.google.com should NOT be in /var/cache/nscd/hosts
+
+# Another set of requests to https://www.google.com/
+
+# Verify
+# - 2 DNS request uncached - increased
+# - Cached entry count == 1
+# - www.google.com should be in /var/cache/nscd/hosts
+
+
+docker ps -a
+
+((docker exec -t docker-client-1 tcpdump -n udp) > client-tcpdump.txt)&
+
+echo 'echo reload-count 0 >> /etc/nscd.conf'  | docker exec -i docker-client-1 su -
+echo '/etc/init.d/nscd start'  | docker exec -i docker-client-1 su -
+sleep 5
+echo '/etc/init.d/nscd status'  | docker exec -i docker-client-1 su -
+
+# Show nscd information before requests are made
+$(dirname "$0")/assert/assert_nscd_cached_entry_count.sh "=="
+
+$(dirname "$0")/assert/assert_nscd_host_entry.sh 0
+
+docker exec -t docker-client-1 sh -c 'echo Google Chrome version : $(echo google-chrome --version | su - ubuntu)'
+
+# First set of requests to https://www.google.com/
+echo 'DISPLAY=:1 python3 /home/ubuntu/selenium/google-resolve-browser-no-cache.py'  | docker exec -i docker-client-1 su - ubuntu
+
+# verify
+$(dirname "$0")/assert/assert_nscd_request_count.sh 1
+$(dirname "$0")/assert/assert_nscd_cached_entry_count.sh ">"
+$(dirname "$0")/assert/assert_nscd_host_entry.sh 1
+
+# invalidate hosts
+echo 'nscd --invalidate=hosts'  | docker exec -i docker-client-1 su -
+
+# verify
+$(dirname "$0")/assert/assert_nscd_request_count.sh 1
+$(dirname "$0")/assert/assert_nscd_cached_entry_count.sh "=="
+$(dirname "$0")/assert/assert_nscd_host_entry.sh 0
+
+# Another set of requests to https://www.google.com/
+echo 'DISPLAY=:1 python3 /home/ubuntu/selenium/google-resolve-browser-no-cache.py'  | docker exec -i docker-client-1 su - ubuntu
+
+# verify
+$(dirname "$0")/assert/assert_nscd_request_count.sh 2
+$(dirname "$0")/assert/assert_nscd_cached_entry_count.sh ">"
+$(dirname "$0")/assert/assert_nscd_host_entry.sh 1
+
+
+echo "Killing tcpdump at - $(date)"
+PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
+docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
+
+echo "Full tcpdump"
+cat client-tcpdump.txt
+
+echo "Only with port 53"
+cat client-tcpdump.txt | grep "\.53:"
