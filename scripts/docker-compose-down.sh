3d501bb23bc1459226d1c33ae4968902d4df44cf
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-15 23:19:14 +1100

    removed * for rsync

--- scripts/docker-compose-down.sh
+++ scripts/docker-compose-down.sh
@@ -6,5 +6,5 @@ docker info | grep "Root"
 mkdir -p /tmp/var-lib-docker
 sudo ls -l /var/lib/docker
 sudo ls -ld /var/lib/docker
-time sudo rsync -ar /var/lib/docker/* /tmp/var-lib-docker
+time sudo rsync -ar /var/lib/docker/ /tmp/var-lib-docker
 sudo chown -R runner /tmp/var-lib-docker
