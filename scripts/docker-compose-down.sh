31b975551bc485ee5f433ae873ad2594ff1fcd8e
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-15 23:48:50 +1100

    dockerx build-push?

--- scripts/docker-compose-down.sh
+++ scripts/docker-compose-down.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 cd docker
 docker-compose down
-
-docker info | grep "Root"
-mkdir -p /tmp/var-lib-docker
-sudo ls -l /var/lib/docker
-sudo ls -ld /var/lib/docker
-time sudo rsync -ar /var/lib/docker/ /tmp/var-lib-docker
-sudo chown -R runner /tmp/var-lib-docker
