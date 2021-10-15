fca6ddd7a90c195f93debb9f1f474fb64d617569
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-16 00:02:38 +1100

    request count 1

--- jobs/verify_nscd_cache_hit.sh
+++ jobs/verify_nscd_cache_hit.sh
@@ -40,7 +40,7 @@ echo "Only with port 53"
 cat client-tcpdump.txt | grep "\.53:"
 
 # verify
-$(dirname "$0")/assert/assert_nscd_request_count.sh 9
+$(dirname "$0")/assert/assert_nscd_request_count.sh 1
 
 # Show nscd information after requests are made
 $(dirname "$0")/assert/assert_nscd_cached_entry_count.sh ">"
