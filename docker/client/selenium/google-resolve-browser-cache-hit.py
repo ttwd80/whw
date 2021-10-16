7b3e36db597fbd7fe26af15ce8983d0548accd95
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-16 18:31:31 +1100

    renamed

--- docker/client/selenium/google-resolve-browser-cache-hit.py
+++ docker/client/selenium/google-resolve-browser-cache-hit.py
@@ -3,14 +3,21 @@ from selenium.webdriver import Chrome
 from time import strftime
 import time
 
+url = "https://www.google.com/favicon.ico"
+
 driver = Chrome()
-url = "https://github.com/favicon.ico"
 print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
 driver.get(url)
-time.sleep(55)
+# driver.quit()
+
+time.sleep(5)
+# driver = Chrome()
 print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
 driver.get(url)
-time.sleep(10)
+# driver.quit()
+
+time.sleep(5)
+# driver = Chrome()
 print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
 driver.get(url)
 driver.quit()
--- jobs/verify_browser_dns_cache_hit.sh
+++ jobs/verify_browser_dns_cache_hit.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash -e
 
 # If a request DNS resolution request has been made in the last 60 seconds, a cached value is used.
+# A request to resolve www.google.com will not be made.
 
 docker ps -a
 
 ((docker exec -t docker-client-1 tcpdump -n udp) > client-tcpdump.txt)&
 
 docker exec -t docker-client-1 sh -c 'echo Google Chrome version : $(echo google-chrome --version | su - ubuntu)'
-echo 'DISPLAY=:1 python3 /home/ubuntu/selenium/github-resolve-browser-cache.py'  | docker exec -i docker-client-1 su - ubuntu
+echo 'DISPLAY=:1 python3 /home/ubuntu/selenium/google-resolve-browser-cache-hit.py'  | docker exec -i docker-client-1 su - ubuntu
 
 PID_TCPDUMP=$(docker exec -t docker-client-1 pidof tcpdump)
 docker exec -t docker-client-1 sh -c "kill $PID_TCPDUMP"
@@ -20,11 +21,11 @@ cat client-tcpdump.txt | grep "53:"
 
 # verify
 echo "Verify DNS resolution count..."
-ACTUAL=$(cat client-tcpdump.txt| grep "github.com" | grep -v api | wc -l | tr -d ' ')
+ACTUAL=$(cat client-tcpdump.txt| grep -w "www.google.com" | wc -l | tr -d ' ')
 # 3 request sent, 2 were cache misses and will hit the DNS resolver
 # - cache miss, first call, cached
 # - cache hit, within one minute of the cached call
 # - cache miss, after one minute of the cached call
 EXPECTED=2
 echo ACTUAL=${ACTUAL}
-echo ${ACTUAL} | grep -w ${EXPECTED}
+echo ${ACTUAL} | grep "^${EXPECTED}\$"
