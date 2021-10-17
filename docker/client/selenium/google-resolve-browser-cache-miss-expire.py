4d24f3252ab4b397ccea6bf512cfd6c8ee8c8b9f
Titi Wangsa Damhore <ttwd80@gmail.com>
2021-10-17 17:23:29 +1100

    should be 3

---
+++ docker/client/selenium/google-resolve-browser-cache-miss-expire.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from selenium.webdriver import Chrome
+from time import strftime
+import time
+
+url = "https://www.google.com/favicon.ico"
+
+driver = Chrome()
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+# driver.quit()
+
+time.sleep(60)
+# driver = Chrome()
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+driver.quit()
+
+time.sleep(60)
+# driver = Chrome()
+print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
+driver.get(url)
+driver.quit()
