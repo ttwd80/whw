6d290b116c7e2ce0a834b16aeab2dcbed4813755
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-22 07:10:29 +1100

    try: no dash dash

--- docker/client/selenium/google-host-resolver-rules.py
+++ docker/client/selenium/google-host-resolver-rules.py
@@ -8,7 +8,7 @@ url = "https://www.google.com/favicon.ico"
 
 options = webdriver.ChromeOptions()
 #  https://source.chromium.org/chromium/chromium/src/+/main:services/network/public/cpp/network_switches.cc?q=kHostResolverRules&ss=chromium
-options.add_argument("--host-resovler-rules=MAP www.google.com 8.8.8.8")
+options.add_argument("host-resovler-rules=MAP www.google.com 8.8.8.8")
 
 driver = Chrome(chrome_options=options)
 print("Request time: " + strftime("%Y-%m-%d %H:%M:%S\n"))
