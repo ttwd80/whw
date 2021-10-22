5768448f0bffddf4e498e085309b2024bcf8840d
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-22 20:41:56 +1100

    fix: made jobs.yaml smaller

--- scripts/client-deb-cache-key.sh
+++ scripts/client-deb-cache-key.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-GOOGLE_CHROME_URL="${1}"
 GOOGLE_CHROME_DRIVER_LISTING_URL="https://chromedriver.chromium.org/downloads"
 HASH_GOOGLE_CHROME_DEB_LAST_MODIFIED=$(curl --silent --head ${GOOGLE_CHROME_URL} | grep -i "last-modified:" | md5sum | cut -b 1-6)
 HASH_GOOGLE_CHROME_DRIVER_LATEST_VERSION=$(curl --silent "${GOOGLE_CHROME_DRIVER_LISTING_URL}" | grep -o 'href="https://[^\"]*"' | grep path | grep -o '[0-9][0-9\.]*' | head -1 | md5sum | cut -b 1-6)
