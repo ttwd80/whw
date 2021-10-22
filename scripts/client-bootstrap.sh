67e4c50f7f567d5f5b85c55802e55dfb70c7ef06
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-23 05:36:17 +1100

    test: enable all

--- scripts/client-bootstrap.sh
+++ scripts/client-bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -i docker-client-1 sh -c 'apt-get update -qq && DEBIAN_FRONTEND=noninteractive apt-get install curl wget coreutils -qq'
+docker exec -i docker-client-1 sh -c 'apt-get update -qq && DEBIAN_FRONTEND=noninteractive apt-get install curl wget coreutils apt-utils -qq'
