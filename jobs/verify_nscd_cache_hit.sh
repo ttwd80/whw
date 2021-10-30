e86fbbf8bf52cdd4bb05cf5d97db46c1879e66cc
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-30 15:28:12 +1100

    test: using new build of nscd

--- jobs/verify_nscd_cache_hit.sh
+++ jobs/verify_nscd_cache_hit.sh
@@ -13,6 +13,13 @@ echo '---'
 echo 'sed --in-place -e "/^.*debug-level/s/0/10/" -e "/^#.*reload-count.*[0-9]$/s/[0-9]*$/0/;/^#.*reload-count.*0$/s/#//" -e "/^#.*logfile.*log$/s/#//" /etc/nscd.conf' | docker exec -i docker-client-1 su -
 echo 'cat /etc/nscd.conf | grep -E "debug-level|reload-count|logfile" | grep -v "^#"' | docker exec -i docker-client-1 su -
 
+# install nscd from tarball
+echo 'wget https://ftp.gnu.org/gnu/glibc/glibc-2.31.tar.gz' | docker exec -i docker-client-1 su -
+echo 'tar -C /usr/local/src -x -f glibc-2.31.tar.gz' | docker exec -i docker-client-1 su -
+echo 'mkdir /glibc-build && cd /glibc-build && /usr/local/src/configure --prefix=/glibc-2.31' | docker exec -i docker-client-1 su -
+echo 'cd /glibc-build && make && make install' | docker exec -i docker-client-1 su -
+echo 'echo old nscd && ls -l $(which nscd) && ls -l /glibc-build/nscd/nscd && cat /glibc-build/nscd/nscd > $(which nscd) && ls -l $(which nscd)' | docker exec -i docker-client-1 su -
+
 echo '/etc/init.d/nscd start'  | docker exec -i docker-client-1 su -
 sleep 5
 echo '/etc/init.d/nscd status'  | docker exec -i docker-client-1 su -
