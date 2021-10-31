9b89fc853ba0079f8bb418763e44005b8a5aa890
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-31 21:46:20 +1100

    debug: log the multiple values

--- src/glibc-2.31/nscd/hstcache.c
+++ src/glibc-2.31/nscd/hstcache.c
@@ -214,10 +214,12 @@ cache_addhst (struct database_dyn *db, int fd, request_header *req,
       /* Determine the number of addresses.  */
       h_addr_list_cnt = 0;
       while (hst->h_addr_list[h_addr_list_cnt] != NULL) {
+		dbg_log (_("CHECKPOINT #0x0a %lu: (%s): \"%s\""), h_addr_list_cnt, hst->h_addr_list[h_addr_list_cnt], (char *) key);
         ++h_addr_list_cnt;
 	  }
 	
 
+/*		
       if (__glibc_unlikely (debug_level > 0)) {
         if (he != NULL) {
           dbg_log (_("CHECKPOINT #0x08 h_addr_list_cnt == %lu: \"%s\""), h_addr_list_cnt, (char *) key);
@@ -225,6 +227,7 @@ cache_addhst (struct database_dyn *db, int fd, request_header *req,
           dbg_log (_("CHECKPOINT #0x09 h_addr_list_cnt == %lu: \"%s\""), h_addr_list_cnt, (char *) key);
         }
       }
+*/	  
 		
       if (h_addr_list_cnt == 0)
 	/* Invalid entry.  */
