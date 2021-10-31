55218987b3d1852dbc79da19322a7586ab93ebe7
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-31 23:15:37 +1100

    test: using quad 9

--- docker/client/scripts/init_etc_resolv_conf.sh
+++ docker/client/scripts/init_etc_resolv_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cat /etc/resolv.conf > /root/resolv.conf.original
 cat /etc/resolv.conf | sed 's/nameserver .*/nameserver 172.23.0.53/' > /root/resolv.conf.custom
-cat /etc/resolv.conf | sed 's/nameserver .*/nameserver 208.67.222.123/' > /root/resolv.conf.opendns
-cat /root/resolv.conf.opendns > /etc/resolv.conf
+cat /etc/resolv.conf | sed 's/nameserver .*/nameserver 9.9.9.9/' > /root/resolv.conf.quad9
+cat /root/resolv.conf.quad9 > /etc/resolv.conf
